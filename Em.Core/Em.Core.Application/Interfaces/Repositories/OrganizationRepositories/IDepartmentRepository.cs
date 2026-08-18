6eddaa87a27f7e971b84289dcad19fd940709f9a
EfeA24 <157731616+EfeA24@users.noreply.github.com>
2026-08-18 08:09:19 +0300

    Repository Interface Complete

---
+++ Em.Core/Em.Core.Application/Interfaces/Repositories/OrganizationRepositories/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+using Em.Core.Application.Interfaces.Generic;
+using Em.Core.Domain.Entities.Organization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Em.Core.Application.Interfaces.Repositories.OrganizationRepositories
+{
+    public interface IDepartmentRepository : IGenericRepository<Department>
+    {
+    }
+}
---
+++ Em.Core/Em.Core.Application/Interfaces/Repositories/TicketsRepositories/ITicketRepository.cs
@@ -0,0 +1,12 @@
+using Em.Core.Application.Interfaces.Generic;
+using Em.Core.Domain.Entities.Tickets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Em.Core.Application.Interfaces.Repositories.TicketsRepositories
+{
+    public interface ITicketRepository : IGenericRepository<Ticket>
+    {
+    }
+}
